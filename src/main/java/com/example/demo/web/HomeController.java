a256129f0a69df96230916362b095fc268cbcac4
steveb <stevejbartholomew@gmail.com>
2021-08-01 17:24:19 +0100

    18

--- src/main/java/com/example/demo/web/HomeController.java
+++ src/main/java/com/example/demo/web/HomeController.java
@@ -4,9 +4,10 @@ import com.example.demo.repository.MarsRoverApiService;
 import com.example.demo.response.MarsRoverApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -15,9 +16,18 @@ public class HomeController {
     private MarsRoverApiService roverService;
 
     @GetMapping("/")
-    public String getHomeView(ModelMap model) {
-        MarsRoverApiResponse roverData =  roverService.getRoverData("curiosity");
+    public String getHomeView (ModelMap model) {
+        MarsRoverApiResponse roverData = roverService.getRoverData("opportunity");
         model.put("roverData", roverData);
+
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String postHomeView (ModelMap model, @RequestParam String marsApiRoverData) {
+        MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData);
+        model.put("roverData", roverData);
+
         return "index";
     }
 }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,10 @@
+let marsApiButtons = document.querySelectorAll("button[id*='marsApi']")
+
+marsApiButtons.forEach(button => button.addEventListener('click', function(){
+
+  const buttonId = this.id
+  const roverId = buttonId.split("marsApi")[1]
+  let apiData = document.getElementById('marsApiRoverData')
+  apiData.value = roverId
+  document.getElementById('formRoverType').submit()
+    }))
